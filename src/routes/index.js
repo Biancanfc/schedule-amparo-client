0679b8d1d2149915793d06f96f66c5da841dc6c3
biancanfc <bianca.nunes1175@gmail.com>
2021-01-24 13:00:18 -0300

    add routes

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import React, { useLayoutEffect } from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import Route from './route';
+import history from './history';
+import Activity from 'pages/Activity';
+
+function Routes() {
+  useLayoutEffect(() => {
+    const unlisten = history.listen(() => {
+      window.scrollTo(0, 0);
+    });
+    return unlisten;
+  }, []);
+
+  return (
+    <BrowserRouter history={history}>
+      <Switch>
+        <Route exact path={'/'} component={Activity} noContainer isPrivate />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
